b9f932a1840b4824545bd9f522cdafd830f68551
GlenWamaru <garpirungu@gmail.com>
2024-04-13 14:57:38 +0300

    Initial commit

--- server/app.py
+++ server/app.py
@@ -22,13 +22,37 @@ def clear_session():
 
 @app.route('/articles')
 def index_articles():
+    articles = Article.query.all()
+    
+    articles_list = [{'id': article.id, 'title': article.title, 'content': article.content} for article in articles]
+    
+    return jsonify(articles_list)
 
-    pass
+
 
 @app.route('/articles/<int:id>')
 def show_article(id):
+      
+    if 'page_views' not in session:
+        session['page_views'] = 0
+    
 
-    pass
+    session['page_views'] += 1
+    
+
+    if session['page_views'] > 3:
+        return jsonify({'message': 'Maximum pageview limit reached.'}), 401 
+    
+    article = Article.query.get(id)
+    
+    if not article:
+        return jsonify({'message': 'Article not found.'}), 404 
+    
+    article_data = {'id': article.id, 'title': article.title, 'content': article.content}
+    return jsonify(article_data)
+
+
+    
 
 if __name__ == '__main__':
     app.run(port=5555)
